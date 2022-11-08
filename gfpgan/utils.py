e0ea30655caefcf7264127638adf752e9154acdd
Lincoln Stein <lincoln.stein@gmail.com>
2022-11-08 00:14:21 -0500

    move location of facexlib weights for compatibility with invokeai

--- gfpgan/utils.py
+++ gfpgan/utils.py
@@ -84,7 +84,7 @@ class GFPGANer():
             save_ext='png',
             use_parse=True,
             device=self.device,
-            model_rootpath='gfpgan/weights')
+            model_rootpath='models/gfpgan/weights')
 
         if model_path.startswith('https://'):
             model_path = load_file_from_url(
